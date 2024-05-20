de6da4d398ce9499d34ed3b53bddac335197586d
oxygen26 <oxygen26@163.com>
2024-05-20 07:19:08 +0000

    test

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 # 创建应用实例
 import sys
 
-from wxcloudrun import app
+#from wxcloudrun import app
 
 # 启动Flask Web服务
+#if __name__ == '__main__':
+#    app.run(host=sys.argv[1], port=sys.argv[2])
+
+import werobot
+ 
+robot = werobot.WeRoBot(token='tokenhere')
+ 
+@robot.text
+def hello_world():
+    return 'Hello World!'
+
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+    robot.run()
