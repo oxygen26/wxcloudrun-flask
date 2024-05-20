33d7920956d0142bd5f8ab4fd2e6988830dfeb70
oxygen26 <oxygen26@163.com>
2024-05-20 10:10:15 +0000

    1

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ from werobot.contrib.flask import make_view
 
 #app = Flask(__name__)
 app.add_url_rule(rule='/robot/', # WeRoBot 的绑定地址
-                endpoint='werobot', # Flask 的 endpoint
+                #endpoint='werobot', # Flask 的 endpoint
                 view_func=make_view(robot),
                 methods=['GET', 'POST'])
 
