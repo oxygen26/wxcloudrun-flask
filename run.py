a8074734c0ddff3fd0b5a778e629ae19cf9a123d
oxygen26 <oxygen26@163.com>
2024-05-20 09:41:35 +0000

    1

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import sys
 
 import werobot
  
-robot = werobot.WeRoBot(token='tokenhere')
+robot = werobot.WeRoBot(app_id='wx20b1396d77813bab',token='AAQ9G7sEAAABAAAAAAAZHBKxAjsPDrr0/hdLZiAAAAAraAdzKm8i8JwFJ68cDOJBtIHsmv3F8e00LCv7f+tYvrdcIYN+n7bDX7DzL30SBV0F41HP29O6/zzd24/PPjeAmEt1eV+92opWZoTZaYXE4803U3e+7eFwGqdjQCxZ5L6SFxz0e5v1XDMlpLF5mQd/oW6tQ3lCcFxS')
  
 @robot.text
 def hello_world():
