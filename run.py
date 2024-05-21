fd5dd820878d7633c809870f535442c215d195c2
oxygen26 <oxygen26@163.com>
2024-05-21 15:49:57 +0000

    1c

--- run.py
+++ run.py
@@ -76,28 +76,96 @@ def check_signature(token, signature, timestamp, nonce):
     hashcode = sha1.hexdigest()
     return hashcode == signature
 
-def json_to_xml(json_data):
-    # 创建 XML 根元素
-    root = ET.Element("xml")
-    
-    # 递归转换 JSON 数据为 XML 元素
-    def build_xml_element(parent, data):
-        if isinstance(data, dict):
-            for key, value in data.items():
-                child = ET.SubElement(parent, key)
-                build_xml_element(child, value)
-        elif isinstance(data, list):
-            for item in data:
-                child = ET.SubElement(parent, "item")
-                build_xml_element(child, item)
-        else:
-            parent.text = str(data)
+# # insertion =======================================================
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core.chat_engine import ContextChatEngine
+import sys,os
+#sys.path.append('/home/oxygen26/llama/python/')
+#from core import llm_y
+# * settings ==========================================================
+from llama_index.core import Settings
 
-    build_xml_element(root, json_data)
-    
-    # 生成 XML 字符串
-    xml_str = ET.tostring(root, encoding='utf-8')
-    return xml_str
+from langchain.llms import QianfanLLMEndpoint
+from llama_index.llms.langchain.base import LangChainLLM
+#from llama_index.llms.openai import OpenAI
+
+#llm_openai = OpenAI(api_key="sk-6zm8Aqt9A31MPRiDV7WJ17EhfsMNjwwApt8PLkmuLZCH8kdo",api_base="https://api.openai-proxy.org/v1",model='gpt-4')
+
+"""For basic init and call"""
+
+# 'Yi-34B-Chat','ERNIE-Bot-4','ERNIE-Speed-128k','ERNIE-Bot-turbo'
+os.environ["QIANFAN_AK"] = "2kFfoTXEOjItYtBAQtVZUv17" #"Q0gXTGHI4oM17zMbI3sMeqIg"#
+os.environ["QIANFAN_SK"] = "s38GyXy4v8xipic8rIlVkm0gTeBL4zkU" #"iWmqKV7A1WSVwatdbmOxK4L5Av1Goxqf"#
+
+qllm_y = QianfanLLMEndpoint(model='Yi-34B-Chat',#'Yi-34B-Chat','ERNIE-Bot-4','ERNIE-Speed-128k','ERNIE-Bot-turbo'
+                           #streaming=True,
+                        ak=os.getenv('QIANFAN_AK'), sk=os.getenv('QIANFAN_SK'),**{'top_p': 0.8, 'temperature': 0.1, 'penalty_score': 1})
+#res = llm("hi")
+qllm_wx = QianfanLLMEndpoint(model='ERNIE-Speed-128k',#'Yi-34B-Chat','ERNIE-Bot-4','ERNIE-Speed-128k','ERNIE-Bot-turbo'
+                           #streaming=True,
+                        ak=os.getenv('QIANFAN_AK'), sk=os.getenv('QIANFAN_SK'),**{'top_p': 0.8, 'temperature': 0.1, 'penalty_score': 1})
+qllm_wx4 = QianfanLLMEndpoint(model='ERNIE-4.0-8K',#'Yi-34B-Chat','ERNIE-Bot-4','ERNIE-Speed-128k','ERNIE-Bot-turbo'
+                           #streaming=True,
+                        ak=os.getenv('QIANFAN_AK'), sk=os.getenv('QIANFAN_SK'),**{'top_p': 0.8, 'temperature': 0.1, 'penalty_score': 1})
+#-----------------------------------------------------
+llm_y = LangChainLLM(llm=qllm_y)
+Settings.llm = llm_y
+
+# messages = [
+#     ChatMessage(
+#         #role="system", content="You are a pirate with a colorful personality"
+#     ),
+#     ChatMessage(role="user", content="hi"),
+# ]
+
+#custom_chat_history = [ChatMessage(role= (MessageRole.USER if i['role']=='user' else MessageRole.ASSISTANT),content=i['content']) for i in session.messages]
+#for wenxin_model in ['Yi-34B-Chat','ERNIE-Bot-4','ERNIE-Speed-128k','ERNIE-Bot-turbo']:
+llama_flag=0
+try:
+                    
+    from llama_index.core.retrievers import QueryFusionRetriever
+    retriever = QueryFusionRetriever(
+        [commit_sctx.std_retriever(), commit_file_sctx.std_retriever()],
+        similarity_top_k=2,
+        num_queries=1,  # set this to 1 to disable query generation
+        use_async=False,#True,
+        verbose=True,
+        # query_gen_prompt="...",  # we could override the query generation prompt here
+    )
+    # query_engine = RetrieverQueryEngine(node_postprocessors=[MetadataReplacementPostProcessor(target_metadata_key="window")],
+    #             retriever=retriever,
+    #             response_synthesizer=synth
+    #             )#.query('生物信息学，给出参考链接')
+    chat_engine = ContextChatEngine.from_defaults(
+        retriever=retriever,#commit_sctx.query_engine(),
+    #condense_question_prompt=custom_prompt,
+        #chat_history=custom_chat_history[:-1],
+        #service_context=service_context,
+        verbose=True
+    )
+    #break
+except Exception as e:
+    print('query engine error::',e)
+    #break
+    try:
+        from llama_index.core.chat_engine import SimpleChatEngine
+
+        chat_engine = SimpleChatEngine.from_defaults(
+            #chat_history=custom_chat_history[:-1],
+        #service_context=service_context,
+        verbose=False)
+    except Exception as e:
+        print('simple engine error::',e)
+
+def chat(messages):
+    response_text = {'result':chat_engine.chat(
+            messages#custom_chat_history[-1].content
+            ).response,"usage":{"total_tokens":2,"completion_tokens":1}}
+    return response_text['result']
+    #llama_flag=1
+        #break
+        #insertion =------------===========================================
+
 @app.route('/wechat', methods=['POST'])
 def wechat():
     # 检查 Content-Type 是否为 application/json
@@ -174,7 +242,11 @@ def handle_xml_data(xml_data):
     app.logger.debug(f'MsgType: {msg_type}')
     app.logger.debug(f'Content: {content}')
 
-    reply_content = '这是回复的消息' if content == '回复文字' else f'收到你的消息：{content}'
+    if content == '回复文字' :
+        reply_content = '这是回复的消息' 
+    
+    else:
+        reply_content=chat(content) #f'收到你的消息：{content}'
     response_xml = generate_reply(from_user, to_user, reply_content)
     
     return response_xml
